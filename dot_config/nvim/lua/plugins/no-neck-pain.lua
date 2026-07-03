2af6ee4289918c51bcfc2a60b7307c3736fe4a1d
Fabricio Jiménez Morales <fabricioajm@gmail.com>
2026-07-03 17:01:18 +0200

    feat: added no neck pain plugin to lazyvim

---
+++ dot_config/nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"shortcuts/no-neck-pain.nvim",
+		version = "*",
+		opts = {
+			width = 120,
+		},
+		keys = {
+			{
+				"<leader>nn",
+				"<cmd>NoNeckPain<cr>",
+				desc = "Toggle No Neck Pain",
+			},
+		},
+	},
+}
